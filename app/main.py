d33f434d7183990768f504fcc5136164446573b0
Derek Wan <d.wan@icloud.com>
2022-05-02 03:16:56 +0800

    Add pyright

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 
@@ -8,19 +9,19 @@ app = FastAPI()
 class Item(BaseModel):
     name: str
     price: float
-    is_offer: bool = None
+    is_offer: bool = False
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root() -> JSONResponse:
+    return JSONResponse({"Hello": "World"})
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+def read_item(*, item_id: int, q: str | None = None) -> JSONResponse:
+    return JSONResponse({"item_id": item_id, "q": q})
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+def update_item(*, item_id: int, item: Item) -> JSONResponse:
+    return JSONResponse({"item_name": item.name, "item_id": item_id})
