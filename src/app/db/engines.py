22ad2af0bd8e0635c82450341bbfb64513482d51
Derek Wan <d.wan@icloud.com>
2023-04-10 20:35:42 +0900

    Make `clients` a strategy

--- src/app/db/engines.py
+++ src/app/db/engines.py
@@ -1,19 +1,27 @@
-from collections.abc import Iterator
+from collections.abc import Callable, Iterator
 
 from beartype import beartype
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
 from app.core.settings import SETTINGS
 
 SQLITE_ENGINE = create_engine(SETTINGS.DB_URL)
-SessionLocal = sessionmaker(bind=SQLITE_ENGINE, autoflush=False, autocommit=False)
 
 
 @beartype
-def yield_sess() -> Iterator[Session]:  # pragma: no cover
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def create_yield_sess(engine: Engine, /) -> Callable[[], Iterator[Session]]:
+    """Create a `yield_sess` iterator for a given engine."""
+    sess_maker = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+    def yield_sess() -> Iterator[Session]:
+        sess = sess_maker()
+        try:
+            yield sess
+        finally:
+            sess.close()
+
+    return yield_sess
+
+
+yield_sess = create_yield_sess(SQLITE_ENGINE)
